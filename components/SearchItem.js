1b0a981f5ebbd2fcfbe8640fdda9f40d8bf31b51
Keiji Suzuki <zuki.ebetsu@gmail.com>
2015-07-18 10:06:52 +0900

    ハイライトの<em>を有効化

--- components/SearchItem.js
+++ components/SearchItem.js
@@ -10,12 +10,12 @@ class SearchItem extends React.Component {
     console.log('SearchItem#render');
     const item = this.props.item;
     const url = `/item/${item.id}?${this.props.cq}`;
-    const ta = item.title_t + (item.author_t ? ' / ' + item.author_t : '');
+    const ta = {__html: item.title_t + (item.author_t ? ' / ' + item.author_t : '')};
     const pub = (item.publisher_txt ? item.publisher_txt.join('; ') : '') + (item.publishDate_ss ? ', ' + item.publishDate_ss[0] : '')
 
     return (
       <tr key={this.props.key}>
-        <td><Link to={url}>{ta}</Link></td>
+        <td><Link to={url} dangerouslySetInnerHTML={ta} /></td>
         <td>{pub}</td>
         <td>{item.callnumber_s}</td>
         <td>{item.shelf_s}</td>
